4938f749b4ad133b00e34272eb65ab89c4927b02
Kim Asendorf <www@kimasendorf.com>
2016-08-14 23:37:35 +0200

    error views
    splitting development and production

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Page Not Found');
   err.status = 404;
   next(err);
 });
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error-dev', {
       message: err.message,
       error: err
     });
